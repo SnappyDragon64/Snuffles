f861638195f4e150fc62f18bd2546b1ff90ddf32
SnappyDragon18 <pg61151@gmail.com>
2021-12-15 19:37:46 +0530

    Speed adjustments

--- src/main/java/mod/schnappdragon/snuffles/common/entity/animal/Snuffle.java
+++ src/main/java/mod/schnappdragon/snuffles/common/entity/animal/Snuffle.java
@@ -30,10 +30,10 @@ public class Snuffle extends Animal {
 
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 2.0D));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 1.5D));
         this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(Items.COOKIE), false));
-        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.25D));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1.1D, Ingredient.of(Items.COOKIE), false));
+        this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
         this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 6.0F));
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
