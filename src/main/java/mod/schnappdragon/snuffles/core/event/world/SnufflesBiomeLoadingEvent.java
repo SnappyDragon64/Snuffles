f4554476b5fc7a4ce151b59ef74a7b139f66a7e3
SnappyDragon18 <pg61151@gmail.com>
2021-12-15 23:14:51 +0530

    Added snuffles to add icy biomes

--- src/main/java/mod/schnappdragon/snuffles/core/event/world/SnufflesBiomeLoadingEvent.java
+++ src/main/java/mod/schnappdragon/snuffles/core/event/world/SnufflesBiomeLoadingEvent.java
@@ -3,7 +3,7 @@ package mod.schnappdragon.snuffles.core.event.world;
 import mod.schnappdragon.snuffles.core.Snuffles;
 import mod.schnappdragon.snuffles.core.registry.SnufflesEntityTypes;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 public class SnufflesBiomeLoadingEvent {
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void modifyBiomes(BiomeLoadingEvent event) {
-        if (Biomes.SNOWY_PLAINS.location().equals(event.getName()))
+        if (event.getCategory() == Biome.BiomeCategory.ICY)
             event.getSpawns().addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(SnufflesEntityTypes.SNUFFLE.get(), 5, 4, 4));
     }
 }
