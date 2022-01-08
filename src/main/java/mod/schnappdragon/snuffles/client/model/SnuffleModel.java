973443989905f9718c95dec002bfc6bcc8333504
SnappyDragon18 <pg61151@gmail.com>
2022-01-08 12:51:02 +0530

    Model stuff

--- src/main/java/mod/schnappdragon/snuffles/client/model/SnuffleModel.java
+++ src/main/java/mod/schnappdragon/snuffles/client/model/SnuffleModel.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import mod.schnappdragon.snuffles.common.entity.animal.Snuffle;
+import net.minecraft.client.model.AgeableListModel;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
@@ -13,10 +14,12 @@ import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.util.Mth;
 
-public class SnuffleModel<T extends Snuffle> extends EntityModel<T> {
+import java.util.Collections;
+
+public class SnuffleModel<T extends Snuffle> extends AgeableListModel<T> {
     private final ModelPart body;
     private final ModelPart tongue;
-    private final ModelPart extra;
+    private final ModelPart horns;
     private final ModelPart rightFrontLeg;
     private final ModelPart leftFrontLeg;
     private final ModelPart rightHindLeg;
@@ -25,7 +28,7 @@ public class SnuffleModel<T extends Snuffle> extends EntityModel<T> {
     public SnuffleModel(ModelPart part) {
         this.body = part.getChild("body");
         this.tongue = this.body.getChild("tongue");
-        this.extra = part.getChild("extra");
+        this.horns = part.getChild("horns");
         this.rightFrontLeg = part.getChild("right_front_leg");
         this.leftFrontLeg = part.getChild("left_front_leg");
         this.rightHindLeg = part.getChild("right_hind_leg");
@@ -38,9 +41,9 @@ public class SnuffleModel<T extends Snuffle> extends EntityModel<T> {
         PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 14.0F, 0.0F, -0.0436F, 0.0F, 0.0F));
         body.addOrReplaceChild("torso", CubeListBuilder.create().texOffs(0, 25).addBox(-9.0F, -6.0F, -10.0F, 18.0F, 11.0F, 20.0F), PartPose.ZERO);
         body.addOrReplaceChild("tongue", CubeListBuilder.create().texOffs(80, 16).addBox(-6.0F, 0.0F, -7.0F, 12.0F, 1.0F, 8.0F), PartPose.offsetAndRotation(0.0F, 4.0F, -10.0F, 0.3927F, 0.0F, 0.0F));
-        PartDefinition extra = partdefinition.addOrReplaceChild("extra", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 14.0F, 0.0F, -0.0436F, 0.0F, 0.0F));
-        extra.addOrReplaceChild("right_horn", CubeListBuilder.create().texOffs(102, 0).addBox(-3.0F, -7.0F, -6.0F, 3.0F, 8.0F, 8.0F), PartPose.offsetAndRotation(-9.0F, -4.0F, -6.0F, 0.0873F, 0.0F, 0.0F));
-        extra.addOrReplaceChild("left_horn", CubeListBuilder.create().texOffs(80, 0).addBox(0.0F, -7.0F, -6.0F, 3.0F, 8.0F, 8.0F), PartPose.offsetAndRotation(9.0F, -4.0F, -6.0F, 0.0873F, 0.0F, 0.0F));
+        PartDefinition horns = partdefinition.addOrReplaceChild("horns", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 14.0F, 0.0F, -0.0436F, 0.0F, 0.0F));
+        horns.addOrReplaceChild("right_horn", CubeListBuilder.create().texOffs(102, 0).addBox(-3.0F, -7.0F, -6.0F, 3.0F, 8.0F, 8.0F), PartPose.offsetAndRotation(-9.0F, -4.0F, -6.0F, 0.0873F, 0.0F, 0.0F));
+        horns.addOrReplaceChild("left_horn", CubeListBuilder.create().texOffs(80, 0).addBox(0.0F, -7.0F, -6.0F, 3.0F, 8.0F, 8.0F), PartPose.offsetAndRotation(9.0F, -4.0F, -6.0F, 0.0873F, 0.0F, 0.0F));
         partdefinition.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-2.5F, 0.0F, -2.5F, 5.0F, 7.0F, 5.0F), PartPose.offsetAndRotation(-4.5F, 17.0F, -5.5F, 0.0F, 0.1309F, 0.0F));
         partdefinition.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(0, 0).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 7.0F, 5.0F), PartPose.offsetAndRotation(4.5F, 17.0F, -5.5F, 0.0F, -0.1309F, 0.0F));
         partdefinition.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-2.5F, 0.0F, -2.5F, 5.0F, 7.0F, 5.0F), PartPose.offsetAndRotation(-4.5F, 17.0F, 5.5F, 0.0F, 0.1309F, 0.0F));
@@ -48,28 +51,21 @@ public class SnuffleModel<T extends Snuffle> extends EntityModel<T> {
         return LayerDefinition.create(meshdefinition, 128, 64);
     }
 
-    public void renderToBuffer(PoseStack poseStack, VertexConsumer consumer, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-        if (this.young) {
-            poseStack.pushPose();
-            poseStack.scale(0.5F, 0.5F, 0.5F);
-            poseStack.translate(0.0D, 1.5F, 0.0D);
-            ImmutableList.of(this.body, this.rightFrontLeg, this.leftFrontLeg, this.rightHindLeg, this.leftHindLeg).forEach((part) -> {
-                part.render(poseStack, consumer, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-            });
-            poseStack.popPose();
-        }
-        else {
-            ImmutableList.of(this.body, this.extra, this.rightFrontLeg, this.leftFrontLeg, this.rightHindLeg, this.leftHindLeg).forEach((part) -> {
-                part.render(poseStack, consumer, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-            });
-        }
+    protected Iterable<ModelPart> headParts() {
+        return Collections.emptyList();
+    }
+
+    protected Iterable<ModelPart> bodyParts() {
+        return ImmutableList.of(this.body, this.horns, this.rightFrontLeg, this.leftFrontLeg, this.rightHindLeg, this.leftHindLeg);
     }
 
     public void setupAnim(Snuffle snuffle, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+        this.horns.visible = !snuffle.isBaby();
+
         this.body.zRot = 0.12F * (snuffle.isFrosting() ?
                         Mth.sin(ageInTicks * 1.2F) :
                         Mth.sin(limbSwing * 0.5F + (float) Math.PI) * limbSwingAmount);
-        this.extra.zRot = this.body.zRot;
+        this.horns.zRot = this.body.zRot;
 
         this.rightHindLeg.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
         this.leftHindLeg.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
