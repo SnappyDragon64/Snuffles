634fa49077144f8a9e4785a29f212688176d9c6c
SnappyDragon18 <pg61151@gmail.com>
2022-07-27 22:11:38 +0530

    Update SnufflesParticleProviders.java

--- src/main/java/mod/schnappdragon/snuffles/client/particle/SnufflesParticleProviders.java
+++ src/main/java/mod/schnappdragon/snuffles/client/particle/SnufflesParticleProviders.java
@@ -7,7 +7,7 @@ import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,11 +15,11 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Snuffles.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class SnufflesParticleProviders {
     @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void registerParticleFactories(ParticleFactoryRegisterEvent event) {
-        registerFactory(SnufflesParticleTypes.SNOWFLAKE.get(), SnufflesSnowflakeParticle.Provider::new);
+    public static void registerParticleProviders(RegisterParticleProvidersEvent event) {
+        registerProvider(SnufflesParticleTypes.SNOWFLAKE.get(), SnufflesSnowflakeParticle.Provider::new);
     }
 
-    private static <T extends ParticleOptions> void registerFactory(ParticleType<T> particle, ParticleEngine.SpriteParticleRegistration<T> factory) {
+    private static <T extends ParticleOptions> void registerProvider(ParticleType<T> particle, ParticleEngine.SpriteParticleRegistration<T> factory) {
         Minecraft.getInstance().particleEngine.register(particle, factory);
     }
 }
